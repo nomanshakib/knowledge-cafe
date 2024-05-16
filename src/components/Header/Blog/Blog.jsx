a496bebb93344fa19183426af9d5e03ac4cd46cc
nomanshakib <nomanshakib.official@gmail.com>
2024-05-16 22:18:30 +0600

    Mark and remove blog from Bookmark

--- src/components/Header/Blog/Blog.jsx
+++ src/components/Header/Blog/Blog.jsx
@@ -10,6 +10,7 @@ const Blog = ({ blog, handleAddToBookmark, handleMarkAsRead }) => {
     posted_date,
     reading_time,
     hashtags,
+    id
   } = blog;
   return (
     <div className="mb-10 space-y-4">
@@ -45,7 +46,7 @@ const Blog = ({ blog, handleAddToBookmark, handleMarkAsRead }) => {
         ))}
       </p>
       <button
-        onClick={() => handleMarkAsRead(reading_time)}
+        onClick={() => handleMarkAsRead(reading_time, id)}
         className="text-purple-800 font-bold underline"
       >
         Mark As Read
--- src/components/Header/Bookmarks/Bookmarks.jsx
+++ src/components/Header/Bookmarks/Bookmarks.jsx
@@ -5,13 +5,13 @@ const Bookmarks = ({ bookmarks, readingTime }) => {
   return (
     <div className="md:w-1/3 bg-gray-300 ml-4 mt-4">
       <div>
-        <h3 className="text-4xl">Reading Time: {readingTime}</h3>
+        <h3 className="text-4xl text-center mt-4">Reading Time: {readingTime}</h3>
       </div>
       <h1 className="text-3xl text-center mt-4">
         Bookmarked Blog : {bookmarks.length}
       </h1>
-      {bookmarks.map((bookmark) => (
-        <Bookmark key={bookmark.id} bookmark={bookmark}></Bookmark>
+      {bookmarks.map((bookmark, idx) => (
+        <Bookmark key={idx} bookmark={bookmark}></Bookmark>
       ))}
     </div>
   );
